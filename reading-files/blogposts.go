eb8f13d02dbf23b297e8430488ec4a989914d51c
ayiko <andrewayiko40@gmail.com>
2025-08-31 13:05:49 +0300

    Refactor Post struct to include Description and update newPost function to read title and description from blog file

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -1,13 +1,15 @@
 package blogposts
 
 import (
+	"bufio"
 	"io"
 	"io/fs"
 	"strings"
 )
 
 type Post struct {
-	Title string
+	Title       string
+	Description string
 }
 
 func PostsFromFs(fileSystem fs.FS) ([]Post, error) {
@@ -29,8 +31,13 @@ func makePostFromFile(fileSystem fs.FS, f fs.DirEntry) Post {
 }
 
 func newPost(blogfile io.Reader) Post {
-	fileContent, _ := io.ReadAll(blogfile)
-	title := strings.TrimPrefix(string(fileContent), "Title: ")
+	// fileContent, _ := io.ReadAll(blogfile)
+	// title := strings.TrimPrefix(string(fileContent), "Title: ")
 
-	return Post{title}
+	scanner := bufio.NewScanner(blogfile)
+	scanner.Scan()
+	title := strings.TrimPrefix(scanner.Text(), "Title: ")
+	scanner.Scan()
+	description := strings.TrimPrefix(scanner.Text(), "Description: ")
+	return Post{title, description}
 }
